653c4607aceaddc7b9d86a76f1430b2b1c6b1d9f
Patrick Douglas <pdouglas.s@hotmail.com>
2024-01-27 16:52:52 -0300

    Construindo chat

--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -1,14 +1,55 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, SafeAreaView } from 'react-native';
 
-export default function Messages( route ) {
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import ChatMessage from '../../components/ChatMessage';
+
+export default function Messages( {route} ) {
 
   const { thread } = route.params;
-  
+  const [messages, setMessages] = useState([]);
+
+  const user = auth().currentUser.toJSON();
+
+  useEffect(() => {
+    const unsubscribeListener = firestore().collection('MESSAGE_THREADS')
+    .doc(thread._id)
+    .collection('MESSAGES')
+    .orderBy('createAt', 'desc')
+    .onSnapshot( querySnapshot => {
+      const messages = querySnapshot.docs.map(doc => {
+        const firebaseData = doc.data()
+
+        const data = {
+          _id: doc.id,
+          text: '',
+          createAt: firestore.FieldValue.serverTimestamp(),
+          ...firebaseData
+        }
+        if(!firebaseData.system){
+          data.user = {
+            ...firebaseData.user,
+            name: firebaseData.user.displayName
+          }
+        }
+        return data;
+      })      
+      setMessages(messages);
+    })
+    return () => unsubscribeListener();
+  }, []);
+
+
   return (
-    <View style={styles.container}>
-      <Text>Messages!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        style={{ width: '100%'}}
+        data={messages}
+        keyExtractor={ item => item._id}
+        renderItem={({item}) => <ChatMessage data={item} /> }
+      />
+    </SafeAreaView>
   );
 }
 
